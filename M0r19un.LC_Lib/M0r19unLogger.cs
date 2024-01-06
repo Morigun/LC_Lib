47e61ec2045c4d02b4fe0a0fdbdc4917a9ef80a6
Morigun <GunGrayv@gmail.com>
2024-01-06 12:25:43 +0300

    first commit

---
+++ M0r19un.LC_Lib/M0r19unLogger.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx.Logging;
+using System;
+
+namespace M0r19un.LC_Lib{
+    public class M0r19unLogger{
+        internal static ManualLogSource logSource;
+
+        public static void Initialize(string modGUID) => M0r19unLogger.logSource = Logger.CreateLogSource(modGUID);
+
+        public static void Log(object message) => M0r19unLogger.logSource.LogInfo($"[{DateTime.Now:dd.MM.yyyy HH:mm:ss}] - {message}");
+
+        public static void LogError(object message) => M0r19unLogger.logSource.LogError($"[{DateTime.Now:dd.MM.yyyy HH:mm:ss}] - {message}");
+
+        public static void LogWarning(object message) => M0r19unLogger.logSource.LogWarning($"[{DateTime.Now:dd.MM.yyyy HH:mm:ss}] - {message}");
+    }
+}
---
+++ M0r19un.LC_Lib/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace M0r19un.LC_Lib
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "M0r19un.LC_Lib";
+        public const string PLUGIN_NAME = "M0r19un.LC_Lib";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
